d91f5886953fbb96b0b7d464e531dd76003788fa
enebin <enebin777@gmail.com>
2025-06-05 12:19:54 +0900

    Add test routing

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,21 +1,38 @@
 package com.enebin
 
-import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
-import io.ktor.server.plugins.calllogging.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import org.slf4j.event.*
+import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            call.respondText("Hello, Router!")
+        }
+        get("/json") {
+            call.respond(Sample("Hello, JSON"))
+        }
+        get("/html") {
+            call.respondText(
+                """
+        <html>
+            <body>
+                <h1>Hello Ktor HTML</h1>
+                <form action="/json" method="get">
+                    <button type="submit">Go to JSON</button>
+                </form>
+            </body>
+        </html>
+        """.trimIndent(),
+                contentType = io.ktor.http.ContentType.Text.Html
+            )
         }
         // Static plugin. Try to access `/static/index.html`
         staticResources("/static", "static")
     }
 }
+
+@Serializable
+data class Sample(val message: String, val id: Int = 1)
