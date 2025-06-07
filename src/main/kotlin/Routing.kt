008828d21e6f6b2dfdec4affcd2c0ce69bdffca8
enebin <enebin777@gmail.com>
2025-06-07 20:56:20 +0900

    #2 Add summary API & mock
    close #2

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,11 +1,12 @@
 package com.enebin
 
 import com.enebin.api.bitcoin.BitcoinApiClient
+import dto.AccountSummaryResponse
+import dto.HealthResponse
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import kotlinx.serialization.Serializable
 
 fun Application.configureRouting() {
     routing {
@@ -17,17 +18,18 @@ fun Application.configureRouting() {
             call.respond(HealthResponse(status = "OK"))
         }
 
+        get("/api/account/summary") {
+            call.respond(AccountSummaryResponse.mock())
+        }
+
         // Static plugin. Try to access `/static/index.html`
         staticResources("/static", "static")
     }
 }
 
-private suspend fun getBitcoinPrice(): String {
+suspend fun getBitcoinPrice(): String {
     val priceIndex = BitcoinApiClient.fetchBitcoinPrice()
     BitcoinApiClient.close()
     return "비트코인 현재가: ${priceIndex.quotes.USD.price} USD\n" +
             "업데이트 시간: ${priceIndex.lastUpdated}"
 }
-
-@Serializable
-data class HealthResponse(val status: String = "OK")
